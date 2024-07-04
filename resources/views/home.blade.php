2945022da122a199621371885f4d84913ba65dae
mirnesacalakovic <calakovicmirnesa@gmail.com>
2024-07-04 16:03:33 +0200

    Homepage fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,13 @@
         <div class="flex items-start justify-center pt-5 h-full">
             {{-- <h1 class="text-white font-palatino text-4xl">Let's talk Art!</h1> --}}
         </div>
-        <div class="flex items-center bg-my-brown text-my-beige hover:bg-my-brown-3 hover:bg-opacity-85 focus:outline-none focus:bg-my-brown-3 transition duration-250 ease-in-out w-250 h-full justify-center pt-5">
-            <h1 class="text-white font-palatino text-4xl">Let's talk Art!</h1>
+        <div class="flex justify-center items-center bg-my-brown text-my-beige hover:bg-my-brown-3 hover:bg-opacity-85 focus:outline-none focus:bg-my-brown-3 transition duration-250 ease-in-out w-250 h-full pt-5">
+            <div>
+                <h1 class="text-white font-palatino text-4xl">Let's talk Art!</h1>
+                <a href="{{ route('theme.index') }}">Go to Themes -></a>
+            </div>
+
         </div>
+
     </div>
 </x-app-layout>
